c799323149aaa25a55db1f67bf6778023a992ccb
murillonb <murillo_099@hotmail.com>
2025-12-29 15:27:58 +0100

    updated script js

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
-// Garante que o código só rode após o HTML carregar
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll('a[href^="#"]');
+// // Garante que o código só rode após o HTML carregar
+// document.addEventListener("DOMContentLoaded", () => {
+//   const links = document.querySelectorAll('a[href^="#"]');
 
-  links.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      const id = this.getAttribute("href");
+//   links.forEach((link) => {
+//     link.addEventListener("click", function (e) {
+//       const id = this.getAttribute("href");
 
-      // Verifica se o link não é apenas "#" e se o alvo existe
-      if (id !== "#" && id.startsWith("#")) {
-        const target = document.querySelector(id);
+//       // Verifica se o link não é apenas "#" e se o alvo existe
+//       if (id !== "#" && id.startsWith("#")) {
+//         const target = document.querySelector(id);
 
-        if (target) {
-          e.preventDefault(); // Só cancela o clique se o alvo existir
-          target.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-          });
-        }
-      }
-    });
-  });
-});
+//         if (target) {
+//           e.preventDefault(); // Só cancela o clique se o alvo existir
+//           target.scrollIntoView({
+//             behavior: "smooth",
+//             block: "start",
+//           });
+//         }
+//       }
+//     });
+//   });
+// });
